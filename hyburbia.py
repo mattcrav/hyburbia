a8523e8f932c149f398b85e0cee691acf348eac3
Matt Craven <mattcrav@gmail.com>
2022-02-13 11:25:36 -0800

    Maneuver Skill

--- hyburbia.py
+++ hyburbia.py
@@ -50,15 +50,18 @@ class Card:
 
 class Skill:
     costs = {
-        'Focus': {'B': 2}
+        'Focus': {'B': 2},
+        'Maneuver': {'G': 2}
     }
 
     affinities = {
-        'Focus': 'Intelligence'
+        'Focus': 'Intelligence',
+        'Maneuver': 'Dexterity'
     }
 
     effects = {
-        'Focus': {'draw': 2}
+        'Focus': {'draw': 2},
+        'Maneuver': {'react': 2}
     }
 
     def __init__(self, name):
@@ -94,10 +97,19 @@ class Skill:
                 return False
         return True
 
-    def activate(self, deck):
+    def activate(self, deck, cards):
         if self.is_paid():
             for e in self.effect:
                 if e == 'draw':
+                    deck.draw(self.effect[e])
+                if e == 'react':
+                    to_remove = []
+                    for n in cards[:self.effect[e]]:
+                        c = deck.hand[n]
+                        to_remove.append(c)
+                    deck.discard += to_remove
+                    for r in to_remove:
+                        deck.hand.remove(r)
                     deck.draw(self.effect[e])
         self.cards = []
                     
@@ -132,7 +144,13 @@ class Deck:
         self.hand = []
         self.draw(5)
         if len([x for x in self.hand if x.type == 'status']) >= 5:
-            self.alive = False
+            return False
+        return True
+
+    def play_card(self, n, skill):
+        c = self.hand.pop(n)
+        self.discard.append(c)
+        skill.cards.append(c)
 
     def display(self, turn):
         print(
@@ -145,24 +163,32 @@ class Deck:
 
 if __name__ == '__main__':
     deck = Deck()
-    skill = Skill('Focus')
+    skills = [Skill('Focus'), Skill('Maneuver')]
     turn = 1
     i = ''
-    while deck.alive:
+    while deck.refresh_hand():
         if i == 'q':
             break
-        skill.cards = []
-        deck.refresh_hand()
+        for s in skills:
+            s.cards = []
         while True:
             deck.display(turn)
             i = input('Action: ')
-            if i in ['q', 'e']:
+            if i in 'qe':
                 break
-            c = deck.hand.pop(int(i)-1)
-            deck.discard.append(c)
-            skill.cards.append(c)
-            skill.activate(deck)
-            print(f'{skill.name} ({skill.cost_text(skill.cost)}) : {skill.cost_text(skill.paid())} {skill.is_paid()}')
+            if i[0] == 's':
+                if len(i) == 1:
+                    for s in skills:
+                        print(f'{s.name} ({s.cost_text(s.cost)}) : {s.cost_text(s.paid())} {s.is_paid()}')
+                else:
+                    sp = i[1:].split('c')
+                    cards = []
+                    if 'c' in i:
+                        cards = [int(x)-1 for x in sp[1]]
+                    skills[int(sp[0])-1].activate(deck, cards)
+            if i[0] == 'p':
+                sp = i[1:].split('s')
+                deck.play_card(int(sp[0])-1, skills[int(sp[1])-1])
         turn += 1
     print('You died.')
 
